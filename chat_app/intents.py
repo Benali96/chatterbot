4703b88403b8c435f6a59e1f1db071711f417584
Benali96 <samar.benali@isimg.tn>
2024-03-10 23:51:26 +0100

    creat

---
+++ chat_app/intents.py
@@ -0,0 +1,79 @@
+intents = [
+    {
+        "tag": "greeting",
+        "patterns": ["Hi", "Hey", "How are you", "Is anyone there?", "Hello", "Good day"],
+        "responses": ["Hey :-)", "Hello, thanks for visiting", "Hi there, what can I do for you?", "Hi there, how can I help?"]
+    },
+    {
+        "tag": "greeting_tunisien",
+        "patterns": ["Slm", "سلام", "Ahla", "Wink cv", "Bnjr", "Nhark zin", "3aslama ", "M3aya chkoun?"],
+        "responses": ["3aslama :-)", "Y3ycheek 3la tala", "Slm, Kifeh najm n3awneek?", "3aslama, Kifeh najem n3awneek?"]
+    },
+    {
+        "tag": "greeting_francais",
+        "patterns": ["Bonjour", "salut", "Bonsoir", "Comment vas-tu ?", "Est ce que quelqu'un est là?"],
+        "responses": ["Bonjour, bienvenue:-)", "Bonjour, merci de votre visite", "Salut, que puis-je faire pour vous ??"]
+    },
+    {
+        "tag": "goodbye",
+        "patterns": ["Bye", "See you later", "Goodbye"],
+        "responses": ["See you later, thanks for visiting", "Have a nice day", "Bye! Come back again soon."]
+    },
+    {
+        "tag": "thanks",
+        "patterns": ["Thanks", "Thank you", "That's helpful", "Thank's a lot!"],
+        "responses": ["Happy to help!", "Any time!", "My pleasure"]
+    },
+    {
+        "tag": "thanks_fr",
+        "patterns": ["Merci", "Merci beaucoup"],
+        "responses": ["Heureux de pouvoir vous aider!", "À tout moment!", "Avec plaisir"]
+    },
+    {
+        "tag": "thanks_tunisie",
+        "patterns": ["Mr6 y3ychek", "Y3ychek "],
+        "responses": ["Mar7ba bik fi koul wa9t", "Fi koul wa9t!"]
+    },
+    {
+        "tag": "items",
+        "patterns": [
+            "Which items do you have?",
+            "What kinds of items are there?",
+            "What do you sell?"
+        ],
+        "responses": [
+            "We sell coffee and tea",
+            "We have coffee and tea"
+        ]
+    },
+    {
+        "tag": "payments",
+        "patterns": [
+            "Do you take credit cards?",
+            "Do you accept Mastercard?",
+            "Can I pay with Paypal?",
+            "Are you cash only?"
+        ],
+        "responses": [
+            "We accept VISA, Mastercard and Paypal",
+            "We accept most major credit cards, and Paypal"
+        ]
+    },
+    {
+        "tag": "delivery",
+        "patterns": ["How long does delivery take?", "When do I get my delivery?"],
+        "responses": ["Delivery takes 2-4 hours", "Takes 2-4 hours "]
+    },
+    {
+        "tag": "delivery_fr",
+        "patterns": ["Combien de temps prend la livraison?", "Quand vais-je recevoir ma livraison ?"],
+        "responses": ["La livraison prend 2 à 4 heures", "Prend 2-4 heures "]
+    },
+    {
+        "tag": "delivery_tn",
+        "patterns": ["9adeh ta5oudh wa9t bech tousel l commande ?", "Wa9teh tousel commande?"],
+        "responses": ["Livreur ywaselhelk bin sa3tin tlatha swaye3", "Entre 2-3 h "]
+    }
+]
+
+ 
--- chat_app/views.py
+++ chat_app/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
+from .intents import intents
 
 # Create your views here.
+
+bot = ChatBot('chatbot', read_only=False, logic_adapters=[
+    {
+
+        'import_path': 'chatterbot.logic.BestMatch',
+        'default_response': 'Désolé, je suis incapable de comprendre ce que cela signifie.',
+        'maximum_sililarity_threshold': 0.90,
+
+
+
+
+     }])
+
+
+list_trainer = ListTrainer(bot)
+
+def train_chatbot(intents):
+    for intent in intents:
+        patterns = intent['patterns']
+        responses = intent['responses']
+        for pattern in patterns:
+            list_trainer.train([pattern] + responses)
+
+train_chatbot(intents)
+
+chatterbotCorpusTrainer =  ChatterBotCorpusTrainer(bot)
+
+def index(request):
+    return render(request, 'bot/index.html')
+
+def specific(request):
+    number = 5
+    return HttpResponse(number)
+
+def getResponse(request):
+    userMessage = request.GET.get('userMessage')
+    response = bot.get_response(userMessage)
+    return HttpResponse(response)
